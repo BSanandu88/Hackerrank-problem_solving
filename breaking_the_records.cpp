745d3eceed02ab788c010a092fa2e4a9f068a4eb
B S Anandu <59107214+BSanandu88@users.noreply.github.com>
2020-06-09 08:02:44 +0530

    Add files via upload

---
+++ breaking_the_records.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int maximum;
+    int minimum;
+    int minCounter = 0;
+    int maxCounter = 0;
+    cin >> maximum;
+    minimum = maximum;
+
+    for (int i = 1; i < n; i++) {
+        int tmp;
+        cin >> tmp;
+        if (tmp > maximum) {
+            maximum = tmp;
+            maxCounter++;
+        }
+
+        if (tmp < minimum) {
+            minimum = tmp;
+            minCounter++;
+        }
+    }
+
+    cout << maxCounter << " " << minCounter;
+
+    return 0;
+}
